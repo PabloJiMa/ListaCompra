0bfe69139c5314feb1f797fe1ea3985c6cd56e57
p.jimenez <pablo.jim.martin@gmail.com>
2021-03-15 16:36:51 +0100

    Instalado AngularMaterial. Clases para el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from  './admin/login/login.component';
+import { RegistroComponent } from  './admin/registro/registro.component';
+import { RecordarPassComponent } from  './admin/recordar-pass/recordar-pass.component';
+import { VerificarEmailComponent } from  './admin/verificar-email/verificar-email.component';
+
+const  routes:  Routes  = [
+  {
+    path:  'admin',
+    component:  LoginComponent,
+    children: [      
+      { path:  'login', component:  LoginComponent},
+      { path:  'registro', component:  RegistroComponent },
+      { path:  'recordar-pass', component:  RecordarPassComponent },
+      { path:  'verificar-email', component:  VerificarEmailComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { LoginComponent } from './admin/login/login.component';
+import { RegistroComponent } from './admin/registro/registro.component';
+import { RecordarPassComponent } from './admin/recordar-pass/recordar-pass.component';
+import { VerificarEmailComponent } from './admin/verificar-email/verificar-email.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    RecordarPassComponent,
+    VerificarEmailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
