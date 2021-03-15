fe0377545db65500364443d101f8ebf1a27e525e
p.jimenez <pablo.jim.martin@gmail.com>
2021-03-15 13:40:42 +0100

    Configuración Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCU4h1VdL1LQnmdVMfVQ12L7M7OZNwv1uc",
+    authDomain: "listacompra-tolo.firebaseapp.com",
+    projectId: "listacompra-tolo",
+    storageBucket: "listacompra-tolo.appspot.com",
+    messagingSenderId: "514939918647",
+    appId: "1:514939918647:web:a172b496f4bb3110bf62b2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCU4h1VdL1LQnmdVMfVQ12L7M7OZNwv1uc",
+    authDomain: "listacompra-tolo.firebaseapp.com",
+    projectId: "listacompra-tolo",
+    storageBucket: "listacompra-tolo.appspot.com",
+    messagingSenderId: "514939918647",
+    appId: "1:514939918647:web:a172b496f4bb3110bf62b2"
+  }
 };
 
 /*
